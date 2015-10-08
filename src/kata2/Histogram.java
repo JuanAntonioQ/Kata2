1312df56c5378dc9bb7576c948c902c13dbc2330
Juan Antonio <Juan Antonio@JuanAntonio-PC>
2015-10-08 10:09:48 +0100

    general commit

--- src/kata2/Histogram.java
+++ src/kata2/Histogram.java
@@ -3,15 +3,17 @@ package kata2;
 import java.util.HashMap;
 
 
-public class Histogram {
-    private final int[] elVector;
+public class Histogram <T> {
+    private final T[] elVector;
 
-    public Histogram(int[] elVector) {
+    public Histogram(T[] elVector) {
         this.elVector = elVector;
     }
+
     
-    public HashMap <Integer, Integer> getHisto(){
-        HashMap<Integer, Integer> histo = new HashMap<>();
+    
+    public HashMap <T, Integer> getHisto(){
+        HashMap<T, Integer> histo = new HashMap<>();
         
         for (int i = 0; i < elVector.length ; i++) {
             
--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -6,15 +6,15 @@ public class Kata2 {
     
     public static void main(String[] args) {
         
-        int[] array = {2,2,11,4,2,6,11,2,3,5,11,5,100};
-        
+        Integer[] array = {2,2,11,4,2,6,11,2,3,5,11,5,100};
+        String[] array1 = {"Ana", "Juan", "Pedro", "Juan", "Pedro", "Juan", "Pedro"};    
         
         Histogram histo = new Histogram(array);
         
-        HashMap <Integer, Integer> histogram = histo.getHisto();
+        HashMap <String, Integer> histogram = histo.getHisto();
         
         
-        for (int key : histogram.keySet()) {
+        for (Object key : histogram.keySet()) {
             System.out.println(key + ":" + histogram.get(key));
         }
         
